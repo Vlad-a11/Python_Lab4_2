898fcd87de0357bb666e23c594363a8efe8a12d0
Yelenich Vladislav <60173865+Vlad-a11@users.noreply.github.com>
2022-11-02 11:56:53 +0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@
 
 import re
 import sys
+import random
+import string
 
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
@@ -19,12 +21,14 @@ class Main(QDialog):
 
         self.btn_solve.clicked.connect(self.solve)
         self.btn_clear.clicked.connect(self.clear)
+        self.btn2.clicked.connect(self.solve2)
 
     def solve(self):
         self.textEdit_words.clear()
         text = self.textEdit_text.toPlainText()  # получаем наш текст
         txt=text.split()
         for s in txt:
+
             self.textEdit_words.insertPlainText(s[::2]+"\n")
 
     def clear(self):
@@ -32,6 +36,13 @@ class Main(QDialog):
         self.textEdit_words.clear()
 
 
+
+
+    def solve2(self):
+        self.textEdit_words.clear()
+        text = self.textEdit_text.toPlainText()  # получаем наш текст
+        for i in text:
+            self.textEdit_words.insertPlainText(i + random.choice(string.ascii_letters))
 def main():
     app = QApplication(sys.argv)
     window = Main()
--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# Лаборторная работа 4.2
+# Python_3_PyQt5_String
 Работа с визуальными тестовыми данными в Python 3 с PyQt5
 
-![Screenshot](Screen.png)
-
-```
-    def solve(self):
-        self.textEdit_words.clear()
-        text = self.textEdit_text.toPlainText()  # получаем наш текст
-        txt=text.split()
-        for s in txt:
-            self.textEdit_words.insertPlainText(s[::2]+"\n")
-            
-         
+![Screenshot](screenshot.png)
